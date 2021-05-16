713feb0b62cfe673f1286a280ee26aff711358f3
JustinLeung1 <justinleung1999@hotmail.com>
2021-05-15 17:54:24 -0700

    stripe checkout works, but cannot figure out details about person paying

--- online-store/index.js
+++ online-store/index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
+const stripe = require("stripe")("sk_test_51Iio2XDyQynlG6QghXMQyA02YlBE3HKUF5gQPHkDYQApUw6QP4lx6pRVCBU6u7CpOQjfQWARw3XxkY13fHtIaEWj00lMadSgqP");
+const bodyParser = require('body-parser');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -19,6 +21,37 @@ app.use(express.static(__dirname + '/public'));
 
 //handblebar setup
 //app.set('view engine', 'ejs');
+
+
+//stripe test
+app.use(express.static("."));
+app.use(express.json());
+
+app.post('/create-checkout-session', async (req, res) => {
+  amount = req.body.price;
+  console.log(amount)
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'Refill card',
+          },
+          unit_amount: Number(amount) * 100,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'http://localhost:8900/home',
+    cancel_url: 'http://localhost:8900/home',
+  });
+
+  res.json({ id: session.id });
+});
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/signup.html'));
@@ -87,6 +120,32 @@ app.get('/logout', (req, res) => {
   res.clearCookie('session');
   res.redirect('/signin');
 });
+
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
+  const event = request.body;
+  console.log(event);
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntent = event.data.object;
+      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+      // Then define and call a method to handle the successful payment intent.
+      // handlePaymentIntentSucceeded(paymentIntent);
+      break;
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object;
+      // Then define and call a method to handle the successful attachment of a PaymentMethod.
+      // handlePaymentMethodAttached(paymentMethod);
+      break;
+    default:
+      // Unexpected event type
+      console.log(`Unhandled event type ${event.type}.`);
+  }
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
+
 
 app.listen(port);
 console.log(`listening on port ${port}`);
